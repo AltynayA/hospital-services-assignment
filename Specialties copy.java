fb2bddded469218d25bcd1362f487a6d98d51682
Altynay <77896777+AltynayA@users.noreply.github.com>
2024-12-25 10:40:46 +0500

    Delete Specialties copy.java

--- Specialties copy.java
+++
@@ -1,5 +0,0 @@
-public class Specialties {
-    public static final String Pediatry = "Pediatry";
-    public static final String Oncology = "Oncology";
-    public static final String Dentistry = "Dentistry";
-}
